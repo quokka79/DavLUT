210dd3e16160f3168df98339f9c794e76c9debae
Dave Williamson <quokka79@users.noreply.github.com>
2016-09-07 11:07:18 +0100

    Add files via upload

---
+++ DavLUT_demo.m
@@ -0,0 +1,59 @@
+% DavLUT_demo - runs a demonstration of the DavLUTgen function
+
+% options
+LUT_bins = 100;         % number of color bins
+BrightnessMin = 0.4;    % reduce brightness of the lowest bin to this factor
+BrightnessMax = 1;      % reduce brightness of the highest bin to this factor
+
+% make the colormap
+CustomColorMap = DavLUTgen(LUT_bins,BrightnessMin,BrightnessMax);
+CustomCMapTitle = 'DavLUT';
+
+% % uncomment to compare against other colormaps
+% CustomColorMap = parula(LUT_bins);
+% CustomCMapTitle = 'MATLAB - Parula';
+% 
+% CustomColorMap = jet(LUT_bins);
+% CustomCMapTitle = 'MATLAB - Jet';
+
+
+% Show RGB values and peaks demo
+figure('Position',[100 100 1200 400]);
+
+% show RGB values
+subplot(1,3,1)
+hold on
+plot(1:LUT_bins,CustomColorMap(:,1),'r')
+plot(1:LUT_bins,CustomColorMap(:,2),'g')
+plot(1:LUT_bins,CustomColorMap(:,3),'b')
+xlabel('Cmap Index')
+ylabel('Intensity')
+axis([0 LUT_bins 0 1])
+title(['RGB Intensity - ',CustomCMapTitle]);
+legend({'R','G','B'},'Location','northeastoutside','FontSize',6,'FontWeight','normal')
+axis square tight
+
+% show demo data using the Peaks function
+subplot(1,3,2)
+peaks(500);
+peaks_surf = findobj('EdgeColor','k');
+set(peaks_surf,'EdgeColor','none');
+title(['Peaks - ',CustomCMapTitle]);
+view(2)
+axis tight square
+colormap(CustomColorMap);
+axis square tight
+colorbar('eastoutside')
+
+subplot(1,3,3)
+peaks(500);
+peaks_surf = findobj('EdgeColor','k');
+set(peaks_surf,'EdgeColor','none');
+title(['Peaks - ',CustomCMapTitle]);
+colormap(CustomColorMap);
+axis square tight
+
+
+% convert to 8-bit RGB values (n.b. check LUT_bins matches your intent!
+% i.e. set to 256 for 8-bit indexed images)
+CustomColorMap_8bit = floor(CustomColorMap * 255);
---
+++ DavLUTgen.m
@@ -0,0 +1,68 @@
+function cmap_DavLUT = DavLUTgen(ColourBins,BrightnessMin,BrightnessMax)
+% Generates a colormap (cmap_DavLUT) which should be colorblind-accessible
+% and greyscale compatible.
+%
+% When called without any arguments, defaults are applied:
+%
+%   ColourBins     Total number of LUT indicies to cover. MATLAB usually
+%                  uses 64 but more gives a smoother gradient.
+%                  Default = 256 bins
+%
+%   BrightnessMin  Brightness of the lowest LUT index will be reduced to
+%                  this value.
+%                  Default = 0.4
+%
+%   BrightnessMax  From BrightnessMin, the intensity will scale linearly up
+%                  to BrightnessMax.
+%                  Default = 1.0
+
+if nargin < 1
+    ColourBins = 256;
+end
+
+if nargin < 2
+    BrightnessMin = 0.4;
+    BrightnessMax = 1;
+else
+    error('Need to supply 0, 1, or 3 arguments!');
+end
+
+LUT_idx = 0:(ColourBins-1);
+
+% generate the red
+Amplitude_Red = 1;
+Freq_Red = 1 / ((ColourBins - 1) * Amplitude_Red);
+Values_Red = cos(2 * pi * Freq_Red * LUT_idx)';
+Values_Red = (Values_Red - min(Values_Red)) / (max(Values_Red) - min(Values_Red)) ;
+
+% generate the green
+Amplitude_Grn = 4;
+Freq_Grn = 1 / ((ColourBins - 1) * Amplitude_Grn);
+Values_G = 1 + cos(2 * pi * Freq_Grn * LUT_idx)';
+Values_G = (Values_G - min(Values_G)) / (max(Values_G) - min(Values_G)) ;
+
+% generate the blue
+Amplitude_Blu = 4;
+Freq_Blue = 1/(ColourBins*Amplitude_Blu);
+Values_B = 1 +sin(2 * pi * Freq_Blue * LUT_idx)';
+Values_B = (Values_B - min(Values_B)) / (max(Values_B) - min(Values_B)) ;
+
+% generate a brightness gradient
+gs_mask = repmat(linspace(BrightnessMin,BrightnessMax,ColourBins)',1,3);
+
+
+cmap_DavLUT = flipud(horzcat(Values_Red,Values_G,Values_B));
+cmap_DavLUT = gs_mask .* cmap_DavLUT;
+
+% % Show RGB values before any intensity correction
+% figure
+% hold on
+% plot(1:64,CustomColorMap(:,1),'r')
+% plot(1:64,CustomColorMap(:,2),'g')
+% plot(1:64,CustomColorMap(:,3),'b')
+% 
+% title('DaveMap')
+% xlabel('Bin ID')
+% ylabel('Intensity')
+% axis([0 255 0 1])
+
